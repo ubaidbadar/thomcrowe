ac04194563a4003171ec8280263349d81ed528d3
Ubaid Badar <m.ubaidbadar@gmail.com>
2022-12-05 23:26:16 +0500

    removed-console-bug

--- src/plugin/lightGalleryOptions.js
+++ src/plugin/lightGalleryOptions.js
@@ -7,7 +7,7 @@ import "lightgallery.js/dist/css/lightgallery.css";
 
 export const PhotoItem = ({ image, thumb }) => (
 
-    <LightgalleryItem src={image} thumb={thumb}>
+    <LightgalleryItem src={image} thumb={thumb} group="">
         <div className="filter-item image">
             <div className="list_inner">
                 <div className="image">
@@ -26,19 +26,3 @@ export const PhotoItem = ({ image, thumb }) => (
     </LightgalleryItem>
 
 );
-
-{/* <li className="image" key={image.id}>
-    <div className="list_inner">
-        <div className="image">
-            <img src="img/thumbs/1-1.jpg" alt="" />
-            <a className="cavani_tm_full_link zoom" >
-                <div className="main" style={{ backgroundImage: `url(${image.image})` }}></div>
-                <span className="icon"><i className="icon-resize-full-alt"></i></span>
-                <div className="details">
-                    <h3>Alla Gorova</h3>
-                    <span>Image</span>
-                </div>
-            </a>
-        </div>
-    </div>
-</li> */}
